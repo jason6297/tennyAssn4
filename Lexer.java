6fe06879417f37ae4425f215ed39a282d0d33ac3
Jason Peavy <32001022+jason6297@users.noreply.github.com>
2019-09-25 12:20:18 -0500

    first file

---
+++ Lexer.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Lexer 
+{
+	//LIST of tokens (operators + separators + keywords)
+	private class Token 
+	{
+		private String lexeme;
+		private String token;
+		
+		
+		public Token(String lex, String t) 
+		{
+			this.lexeme = lex;
+			this.token = t;
+		}
+		public String getToken() 
+		{
+			return token;
+		}
+		public String getLexeme() 
+		{
+			return lexeme;
+		}
+	}
+	private class TokenError extends Token
+	{
+
+		public TokenError(String lex, String t) 
+		{
+			super(lex, t);
+			// TODO Auto-generated constructor stub
+		}
+		//a special pseudo-token class called "error". 
+		//For example, the ampersand symbol (&) does 
+		//not match any of the language's tokens 
+		//(outside of a comment or string literal). 
+		//Therefore, if the "lex" function encounters 
+		//an ampersand, then the erroneous lexeme "&" 
+		//would be returned with the token descriptor "error".
+	}
+//	public Lexer() 
+//	{
+//		
+//	}
+	public Token lex() 
+	{
+		Scanner scan = new Scanner(System.in);
+		String token= ""; 
+		token = scan.next();
+		//Token = new Token(token, whatitdo(token));
+		return null;
+	}
+	private String Whatitdo(String lex) 
+	{
+		return null;
+	}
+	public static void main(String[] args)
+	{
+//        Lexer lex = new Lexer(args[0]);
+//        Token current = lex.lex();
+//        while(current!=null)
+//        {
+//            System.out.println(current);
+//            current = lex.lex();
+//        }
+	}
+}
